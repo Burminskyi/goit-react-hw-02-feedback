164c64e55f806e90dd2f3faefb0c31eb78050dfe
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-06-20 15:44:49 +0300

    1st part of HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics.jsx';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
+import { Section } from './Section/Section.jsx';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    return Math.round((100 / (good + neutral + bad)) * good);
+  };
+
+  handleBtnClick = e => {
+    const name = e.target.name;
+    this.setState(prev => ({ [name]: prev[name] + 1 }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positiveFeedbacks = this.countPositiveFeedbackPercentage();
+
+    return (
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={this.state}
+          handleBadClick={this.handleBadClick}
+          handleNeutralClick={this.handleNeutralClick}
+          handleGoodClick={this.handleGoodClick}
+          onLeaveFeedback={this.handleBtnClick}
+        />
+        <h3>Statistics</h3>
+        {total === 0 ? (
+          <p>There is no feedback</p>
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedbacks={positiveFeedbacks}
+          />
+        )}
+      </Section>
+    );
+  }
+}
